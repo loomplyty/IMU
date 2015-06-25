2b006bffb93e8a9d682d0d4b9b324768a7aff4ce
hex <hex@hex.com>
2015-06-25 10:46:46 +0800

    IMU first commit

---
+++ build/CMakeFiles/IMU.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/IMU.dir/deviceclass.cpp.o"
+  "CMakeFiles/IMU.dir/conio.c.o"
+  "lib/libIMU.pdb"
+  "lib/libIMU.a"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang C CXX)
+  INCLUDE(CMakeFiles/IMU.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ build/CMakeFiles/demo_IMU.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/demo_IMU.dir/demo_IMU/demo_IMU.cpp.o"
+  "bin/demo_IMU.pdb"
+  "bin/demo_IMU"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/demo_IMU.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
